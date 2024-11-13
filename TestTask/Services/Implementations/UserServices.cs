25c1a251a3887544c3af1968b8ce2f319a4ebba4
lixhern <andreilisai1234@gmail.com>
2024-11-13 23:44:34 +0300

    final commit2

--- TestTask/Services/Implementations/UserServices.cs
+++ TestTask/Services/Implementations/UserServices.cs
@@ -18,23 +18,10 @@ namespace TestTask.Services.Implementations
 
         async Task<User> IUserService.GetUser()
         {
-
-            /*return await _context.Users
+            return await _context.Users
                 .Where(u => u.Orders.Any(o => o.CreatedAt.Year == 2003))
                 .OrderByDescending(u => u.Orders.Where(o => o.CreatedAt.Year == 2003).Sum(o => o.Price))
-                .FirstOrDefaultAsync();*/
-
-            var userWithMaxWaste = await _context.Users
-                .Where(o => o.Orders.Any(o => o.CreatedAt.Year == 2003))
-                .Select(user => new
-                {
-                    id = user.Id,
-                    waste = user.Orders.Sum(o => o.Price),
-                })
-                .OrderByDescending(u => u.waste)
                 .FirstOrDefaultAsync();
-
-            return await _context.Users.FindAsync(userWithMaxWaste.id);
         }
 
         async Task<List<User>> IUserService.GetUsers()
