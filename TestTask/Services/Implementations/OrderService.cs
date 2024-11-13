06e656900c51674672c95332221e51bb9519de51
lixhern <andreilisai1234@gmail.com>
2024-11-13 22:45:21 +0300

    Added orderService with getuser

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        async Task<Order> IOrderService.GetOrder()
+        {
+            return await _context.Orders.FirstOrDefaultAsync();
+        }
+
+        async Task<List<Order>> IOrderService.GetOrders()
+        {
+            return await _context.Orders
+                .Where(u => u.User.Id == 1)
+                .ToListAsync();
+        }
+    }
+}
